6009d5d59bb6b7a9ee37b2f99f5fc74c99e153e7
abbguslof <gustav.lofqvist@abbgymnasiet.se>
2022-11-09 13:40:49 +0100

    +gamefiles

--- Assets/Scripts/Player/SinkShip.cs
+++ Assets/Scripts/Player/SinkShip.cs
@@ -9,13 +9,11 @@ public class SinkShip : MonoBehaviour
         //Check for a match with the specific tag on any GameObject that collides with your GameObject
         if (collision.gameObject.tag == "Boat")
         {
-            GameObject.FindGameObjectsWithTag("MainCamera").GetComponent<SmoothCameraFollow>() = false;
             collision.GetComponent<BoatMovement>().FloatStrengthY = 10;
         }
         if (collision.gameObject.tag == "CanonBall")
         {
-            //If the GameObject has the same tag as specified, output this message in the console
-            Debug.Log("blaasdasdasdbla");
+            Destroy(collision);
         }
         Debug.Log("Do something else here");
     }
